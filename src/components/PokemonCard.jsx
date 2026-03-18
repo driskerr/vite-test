7145872dee5839de40782293d5e00c0d12a9f1bc
driskerr <7308323+driskerr@users.noreply.github.com>
2026-03-18 13:09:54 -0700

    try again

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import StatBar from "./StatBar";
 import { typeData } from "../data/pokemon";
 
 export default function PokemonCard({ pokemon, maxHp, maxAttack, onClick }) {
   const { color, textColor, emoji } = typeData[pokemon.type];
+  const [hovered, setHovered] = useState(false);
 
-  const spriteUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`;
+  const staticUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`;
+  const animatedUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemon.id}.gif`;
 
   return (
     <div
       className="pokemon-card"
       onClick={onClick}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
       style={{
         border: `2px solid ${color}`,
         background: `linear-gradient(to bottom, ${color}90 0%, ${color}22 100%)`,
@@ -20,13 +25,18 @@ export default function PokemonCard({ pokemon, maxHp, maxAttack, onClick }) {
         {emoji} {pokemon.type}
       </p>
       <div className="card-sprite-frame" style={{ border: `1.5px solid ${color}` }}>
-        <img src={spriteUrl} alt={pokemon.name} />
+        <img
+          src={hovered ? animatedUrl : staticUrl}
+          alt={pokemon.name}
+          onError={(e) => { e.target.src = staticUrl; }}
+        />
       </div>
       <h3 className="card-name" style={{ color: textColor }}>
         {pokemon.name}
       </h3>
       <StatBar value={pokemon.hp} max={maxHp} color="#228B22" emoji="❤️" />
       <StatBar value={pokemon.attack} max={maxAttack} color="#8B0000" emoji="⚔️" />
+      {hovered && <div className="card-tooltip">Click for details</div>}
     </div>
   );
 }
